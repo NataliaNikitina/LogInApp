3b3d399038ec2864a1636b1a53f079c416ffa910
Akuna Natata <nne241823@gmail.com>
2023-01-15 00:21:23 +0300

    Initial Commit

---
+++ LogInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInApp
+//
+//  Created by Наталья Никитина on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
